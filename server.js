6adccd6276c20858ce3c438f990dab89df881231
Dimitar Jilanov <djilanov@gmail.com>
2019-02-16 19:01:55 +0200

    Remove wrong console logs and fix long numbers

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require('fs');
  
 cron.schedule('0 1 * * *', () => {
 	const times = Math.floor(Math.random() * Math.floor(13));
-	const long = Math.floor(10000000000000 + Math.random() * 90000000000000) + '';
+	const long = Math.floor(1e13 + Math.random() * 9e13) + '';
 	const charArray = long.split('');
 	for(let counter = 0; counter < times; counter++) {
 		fs.readFile('../spam/spam.txt', 'utf-8', (err, data) => {
@@ -14,14 +14,11 @@ cron.schedule('0 1 * * *', () => {
 					exec('git --git-dir ../spam/.git --work-tree=../spam add .', () => {
 						exec(`git --git-dir ../spam/.git --work-tree=../spam commit -m"Push at ${Math.floor(Math.random() * Math.floor(13))}"`, () => {
 							exec('git --git-dir ../spam/.git --work-tree=../spam push', () => {
-								console.log('error: ', error);
-								console.log('stdout: ', stdout);
-								console.log('stderr: ', stderr);
 								console.log('Done');
 							});
 						});
 					});
-				}, (600000 + +charArray[counter] + +charArray[counter]/10) * 60000);
+				}, (6e5 + +charArray[counter] + +charArray[counter]/10) * 6e4);
 			});
 		});
 	}
