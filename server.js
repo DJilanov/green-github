927ee20cf039d8081ae9cfe1277070aabb02b4e9
Dimitar Jilanov <djilanov@gmail.com>
2019-02-18 11:26:43 +0200

    Fix the numbers to be 32 bit

--- server.js
+++ server.js
@@ -3,14 +3,13 @@ const exec = require('child_process').exec;
 const fs = require('fs');
  
 cron.schedule('0 1 * * *', () => {
-	const times = Math.floor(Math.random() * Math.floor(13));
-	const long = Math.floor(1e13 + Math.random() * 9e13) + '';
-	const charArray = long.split('');
+	const times = Math.floor(Math.random() * 13);
+	const randomCharArray = (Math.floor(1e5 + Math.random() * 9e5) + '' + Math.floor(1e5 + Math.random() * 9e5)).split(''); // you must use 32 bit integers in NodeJS
 	for(let counter = 0; counter < times; counter++) {
 		fs.readFile('../spam/spam.txt', 'utf-8', (err, data) => {
 			fs.writeFile('../spam/spam.txt', Math.random() + '', 'utf-8', (err) => {
 				setTimeout(() => {
-					console.log('Activated at ' + 10 + +charArray[counter] + +charArray[counter]/10);
+					console.log('Activated at ' + 10 + +randomCharArray[counter] + +randomCharArray[counter]/10);
 					exec('git --git-dir ../spam/.git --work-tree=../spam add .', () => {
 						exec(`git --git-dir ../spam/.git --work-tree=../spam commit -m"Push at ${Math.floor(Math.random() * Math.floor(13))}"`, () => {
 							exec('git --git-dir ../spam/.git --work-tree=../spam push', () => {
@@ -18,7 +17,11 @@ cron.schedule('0 1 * * *', () => {
 							});
 						});
 					});
-				}, (6e5 + +charArray[counter] + +charArray[counter]/10) * 6e4);
+				}, 
+					36e6 + // 10 hours
+					+randomCharArray[counter] * 36e5 + // the random num as hours
+					+randomCharArray[counter]/10 // second number min as minutes
+				);
 			});
 		});
 	}
