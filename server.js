d1e4dc6128e5118a60475d8ff3ea6b4d8dfa35c3
Dimitar Jilanov <djilanov@gmail.com>
2019-02-21 12:03:36 +0200

    Add more logers

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ const fs = require('fs');
 
 let gitPusher = () => {
 	const times = Math.floor(Math.random() * 13);
+	console.log('Times: ', times);
 	const randomCharArray = (Math.floor(1e7 + Math.random() * 9e7) + '' + Math.floor(1e7 + Math.random() * 9e7)).split(''); // you must use 32 bit integers in NodeJS
+	console.log('Char array: ', randomCharArray);
 	for(let counter = 0; counter < times; counter++) {
 		fs.readFile('../spam/spam.txt', 'utf-8', () => {
 			fs.writeFile('../spam/spam.txt', Math.random() + '', 'utf-8', () => {
 				setTimeout(() => {
-					console.log('Activated at ' + 10 + +randomCharArray[counter] + +randomCharArray[counter]/10);
+					console.log('Activated at: ', (new Date()));
 					exec('git --git-dir ../spam/.git --work-tree=../spam add .', () => {
 						exec(`git --git-dir ../spam/.git --work-tree=../spam commit -m"Push at ${Math.floor(Math.random() * Math.floor(13))}"`, () => {
 							exec('git --git-dir ../spam/.git --work-tree=../spam push', () => {
@@ -29,6 +31,7 @@ let gitPusher = () => {
  
 cron.schedule('0 1 * * *', () => {
 	gitPusher();
+	console.log('Activate the pushed by the cron');
 }, {
 	scheduled: true,
 	timezone: "America/Sao_Paulo"
